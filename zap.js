d2fa1c60de3c6b22dd2bfafb06f16d20120a8ea4
Julien Quint <julien@igel.co.jp>
2012-07-16 21:45:18 +0900

    Fixed velocity bug

--- zap.js
+++ zap.js
@@ -370,8 +370,8 @@
         if (!isNaN(v)) {
           velocity = zap.clamp(v, 0, this.max_velocity || Infinity);
           var th = this.a / 180 * Math.PI;
-          this.vx = v * Math.cos(th);
-          this.vy = v * Math.sin(th);
+          this.vx = velocity * Math.cos(th);
+          this.vy = velocity * Math.sin(th);
         }
       } });
 
