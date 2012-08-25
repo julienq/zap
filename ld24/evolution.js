f5764079ec33d314b3d878f45d47983a5db57a03
Julien Quint <julien@igel.co.jp>
2012-08-26 08:14:42 +0900

    More ghosts; outlines

--- ld24/evolution.js
+++ ld24/evolution.js
@@ -1,16 +1,17 @@
 (function () {
   "use strict";
 
+  // Custom sprite classes for the eyes and the ghosts
+  $.eyes = Object.create(zap.sprite);
+  $.ghost = Object.create(zap.sprite);
+
   var W, H;
 
-  function floating(dt) {
-    this.x += zap.random_number(-dt * $FLOATING, dt * $FLOATING);
-    this.y += zap.random_number(-dt * $FLOATING, dt * $FLOATING);
-  }
 
-  $.draggable = Object.create(zap.sprite);
+  // Eyes
 
-  $.draggable.init = function (elem) {
+  // Initialize events for the eyes
+  $.eyes.init = function (elem) {
     var target = elem.querySelector(".target");
     target.addEventListener("touchstart", this, false);
     target.addEventListener("touchmove", this, false);
@@ -18,10 +19,12 @@
     target.addEventListener("mousedown", this, false);
     document.addEventListener("mousemove", this, false);
     document.addEventListener("mouseup", this, false);
+    this.invicibility = $EYES_INVICIBILITY;
     return zap.sprite.init.call(this, elem);
   };
 
-  $.draggable.handleEvent = function (e) {
+  // Drag the eyes around when they are not in a ghost
+  $.eyes.handleEvent = function (e) {
     if (e.type === "mousedown" || e.type === "touchstart") {
       if (!this.ghost) {
         this.p = zap.point_from_event(e);
@@ -40,79 +43,9 @@
     }
   };
 
-
-  $.ghost = Object.create(zap.sprite);
-
-  $.ghost.init = function (elem) {
-    elem.setAttribute("fill", zap.random_element($GHOST_COLOR));
-    elem.setAttribute("fill-opacity", zap.random_number($GHOST_OPACITY));
-    this.frame = 0;
-    this.next_frame = $GHOST_FRAME_DUR;
-    this.shape = elem.appendChild($use("#ghost-0"));
-    elem.appendChild($use("#eye-sockets"));
-    zap.sprite.init.call(this, elem);
-    this.v = zap.random_number($GHOST_V);
-    this.h = 180;
-    this.x = $GHOST_X;
-    this.y = zap.random_number($GHOST_Y);
-    return this;
-  };
-
-  $.ghost.did_update = function (dt) {
-    this.next_frame -= dt;
-    if (this.next_frame < 0) {
-      this.next_frame = $GHOST_FRAME_DUR;
-      this.frame = 1 - this.frame;
-      this.shape.setAttributeNS(zap.XLINK_NS, "href",
-          "#ghost-{0}".fmt(this.frame));
-    }
-    if (this.x < W - $GHOST_X) {
-      if (cosmos.$eyes.ghost === this) {
-        delete cosmos.$eyes.ghost;
-        cosmos.$eyes.x = W / 2;
-        cosmos.$eyes.invicible();
-      }
-      this.remove_self();
-    }
-  };
-
-
-  var cosmos = Object.create(zap.cosmos).init();
-  cosmos.next_ghost = zap.random_number($GHOST_PERIOD);
-  var vb = cosmos.elem.viewBox.baseVal;
-  W = vb.width;
-  H = vb.height;
-
-  // Float then see if we capture the eyes
-  $.ghost.will_update = function (dt) {
-    floating.call(this, dt);
-    if (!cosmos.$eyes.ghost && cosmos.$eyes.invicibility < 0 &&
-        cosmos.$eyes.a <= 0 && zap.dist(this, cosmos.$eyes) < $GHOST_RADIUS) {
-      cosmos.$eyes.ghost = this;
-      zap.play_sound("catch-sound", $VOLUME);
-      delete cosmos.$eyes.p;
-      cosmos.$eyes.a = 0;
-      cosmos.$eyes.v = 0;
-    }
-  };
-
-  cosmos.will_update = function (dt) {
-    this.next_ghost -= dt;
-    if (this.next_ghost < 0) {
-      this.next_ghost = zap.random_number($GHOST_PERIOD);
-      this.$ghosts.append_child(Object.create($.ghost).init($g()));
-    }
-  };
-
-  cosmos.$eyes.invicible = function () {
-    zap.play_sound("back-sound", $VOLUME);
-    this.invicibility = $EYES_INVICIBILITY;
-    this.elem.setAttribute("fill-opacity", 0.5);
-  };
-
-  cosmos.$eyes.invicible();
-
-  cosmos.$eyes.will_update = function (dt) {
+  // Update the eyes: check invicibility status; wrap around the screen; follow
+  // a ghost when captured
+  $.eyes.will_update = function (dt) {
     if (this.invicibility > 0) {
       this.invicibility -= dt;
       if (this.invicibility <= 0) {
@@ -134,6 +67,92 @@
         this.x = this.ghost.x;
         this.y = this.ghost.y;
       }
+    }
+  };
+
+  $.eyes.invicible = function () {
+    zap.play_sound("back-sound", $VOLUME);
+    this.invicibility = $EYES_INVICIBILITY;
+    this.elem.setAttribute("fill-opacity", 0.5);
+  };
+
+  $.eyes.capture = function (ghost) {
+    this.ghost = ghost;
+    zap.play_sound("catch-sound", $VOLUME);
+    delete this.p;
+    this.a = 0;
+    this.v = 0;
+    return this;
+  };
+
+  $.eyes.release = function () {
+    delete this.ghost;
+    cosmos.$eyes.x = W / 2;
+    cosmos.$eyes.invicible();
+  };
+
+
+  // Create a ghost with random color/position
+
+  $.ghost.init = function (elem) {
+    elem.setAttribute("stroke", zap.random_element($GHOST_COLOR));
+    elem.setAttribute("stroke-opacity", zap.random_number($GHOST_OPACITY));
+    this.frame = 0;
+    this.next_frame = $GHOST_FRAME_DUR;
+    this.shape = elem.appendChild($use("#ghost-0"));
+    // elem.appendChild($use("#eye-sockets"));
+    // elem.appendChild($use("#nose"));
+    // elem.appendChild($use("#mouth"));
+    // elem.appendChild($use("#moustache"));
+    // elem.appendChild($use("#goatee"));
+    zap.sprite.init.call(this, elem);
+    this.v = zap.random_number($GHOST_V);
+    this.h = 180;
+    this.x = $GHOST_X;
+    this.y = zap.random_number($GHOST_Y);
+    return this;
+  };
+
+  $.ghost.did_update = function (dt) {
+    this.next_frame -= dt;
+    if (this.next_frame < 0) {
+      this.next_frame = $GHOST_FRAME_DUR;
+      this.frame = 1 - this.frame;
+      this.shape.setAttributeNS(zap.XLINK_NS, "href",
+          "#ghost-{0}".fmt(this.frame));
+    }
+    if (this.x < W - $GHOST_X) {
+      if (this.eyes) {
+        this.eyes.release();
+        delete this.eyes;
+      }
+      this.remove_self();
+    }
+  };
+
+
+  // Float then see if we capture the eyes
+  $.ghost.will_update = function (dt) {
+    this.x += zap.random_number(-dt * $FLOATING, dt * $FLOATING);
+    this.y += zap.random_number(-dt * $FLOATING, dt * $FLOATING);
+    if (!cosmos.$eyes.ghost && cosmos.$eyes.invicibility < 0 &&
+        cosmos.$eyes.a <= 0 && zap.dist(this, cosmos.$eyes) < $GHOST_RADIUS) {
+      this.eyes = cosmos.$eyes.capture(this);
+    }
+  };
+
+  // Initialize the cosmos
+  var cosmos = $.cosmos = Object.create(zap.cosmos).init();
+  cosmos.next_ghost = zap.random_number($GHOST_PERIOD);
+  var vb = cosmos.elem.viewBox.baseVal;
+  W = vb.width;
+  H = vb.height;
+
+  cosmos.will_update = function (dt) {
+    this.next_ghost -= dt;
+    if (this.next_ghost < 0) {
+      this.next_ghost = zap.random_number($GHOST_PERIOD);
+      this.$ghosts.append_child(Object.create($.ghost).init($g()));
     }
   };
 
